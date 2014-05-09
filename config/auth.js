b313a96934798626b2da843bd7f4b90f57c40b77
Tomas <tomasbuzek@gmail.com>
2014-05-10 00:38:33 +0200

    Changed oAuth callback for OpenShift.

--- config/auth.js
+++ config/auth.js
@@ -3,8 +3,7 @@ module.exports = {
 	'googleAuth' : {
 		'clientID' 		: '381269499811-ssu9ia2ta84o07a7enc6g6ffbqqcs1vp.apps.googleusercontent.com',
 		'clientSecret' 	: 'gQob9i-x3XYcmQNN_LkBGlxo',
-		/*'callbackURL' 	: 'http://texto-straka.rhcloud.com/auth/google/callback'*/
-		'callbackURL' 	: 'http://localhost:8000/auth/google/callback'
+		'callbackURL' 	: 'http://texto-straka.rhcloud.com/auth/google/callback'
 	}
 
 };
